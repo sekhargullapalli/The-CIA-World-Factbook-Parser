3534be278048d109966bcaaddcff531c5bcdf8ae
Vijaya Sekhar Gullapalli <sekhargullapalli@gmail.com>
2019-04-21 12:39:36 +0200

    added console application for data parsing

---
+++ cia.factbook.parse/cia.factbook.parse/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cia.factbook.parse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Country c = new Country();
+            
+            
+        }
+    }
+}
